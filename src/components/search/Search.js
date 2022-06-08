daec15ea58d3dbd73d385146a40f6e3632dac8a1
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-08 10:48:16 +0200

    feat(search) : ajout de arrow

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,10 +3,12 @@ import Form from '../form/Form';
 import FormMoreOptions from '../formMoreOptions/FormMoreOptions';
 import MoreOptions from '../moreOptions/MoreOptions';
 import './search.scss';
+import Arrow from '../../img/downCurvedArrow.png';
 
 const Search = () => {
 	return (
 		<div className="search">
+			<img className="arrowDown" src={Arrow} alt="" />
 			<h2>Trouvez le professionnel qui vous correspond</h2>
 			<Form />
 			<FormMoreOptions />
